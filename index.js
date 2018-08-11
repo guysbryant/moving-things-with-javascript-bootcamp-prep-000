a151afadc44066872147bc61541f0df12a6519f8
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 21:05:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 $( document ).ready(function(){
-var dodger = $('#dodger')
-dodger.style('background-color', '#111');
+// var dodger = $('#dodger')
+// dodger.style('background-color', '#111');
 console.log(dodger);
 
 
