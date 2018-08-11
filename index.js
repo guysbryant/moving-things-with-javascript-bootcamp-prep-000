5fd40103ece59cd7c106f5cd844667ac3b2be6fd
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 21:23:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(function(){
   var dodger = document.getElementById('dodger');
-  // dodger[0].style.backgroundColor = 'black';
-  console.log(dodger.style);
+  // console.log(dodger.style);
   function moveDodgerLeft(){
     //dodger
   }
