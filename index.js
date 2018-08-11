db277bdebdac23d5300cc4702e25770d2add4791
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 21:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function(){
-  var dodger = $querySelector('#dodger');
+  var dodger = querySelector('#dodger');$
   // dodger[0].style.backgroundColor = 'black';
   console.log(dodger);
   function moveDodgerLeft(){
