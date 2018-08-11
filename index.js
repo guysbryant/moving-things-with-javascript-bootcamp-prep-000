bf2f54748363b543ea3a9fcdee2f62e138dc6e4c
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 21:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ $(function(){
   })
   
   function moveDodgerLeft(){
-    // console.log(parseInt(dodger.style.left));
     var currentPosition = parseInt(dodger.style.left);
     dodger.style.left = `${currentPosition - 10}px`;
   }
