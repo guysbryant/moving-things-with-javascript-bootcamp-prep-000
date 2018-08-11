f5c3c12ba333d8e418ed2dfe5de6d3a0e3cb17ee
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-11 21:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(function(){
   var dodger = document.getElementById('dodger');
-  // console.log(dodger.style.left);
   
   document.addEventListener('keydown', function(e){
     if (e.key === 'ArrowLeft'){
